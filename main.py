377d2c212ff06aa8bcaf40634abac6f778fee3f3
Divya <digupta@q3tech.com>
2018-06-21 16:34:35 +0530

    Remove files.

--- main.py
+++
@@ -1,7 +0,0 @@
-from Enhancement import *
-
-if __name__ == '__main__':
-    #name of the image to be enhanced and cropped 
-    enhancement = Enhancement('3.png','CBSE',10)
-    enhancement.cropping()
-    
